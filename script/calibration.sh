3aea215866881d25405a6e654cf49b76f7dde888
Kenneth "Baba Saitaandas" Goveas <kenneth.goveas@gmail.com>
2025-04-08 22:56:33 +0200

    Added calibration script
    Added `calibration` script for calibrating sensors.

---
+++ script/calibration.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+cd "$(dirname "$0")/.."
+
+interrupted=false
+trap interrupted=true SIGINT
+
+calibrator_command='./build/unit/calibrator'
+
+if [ "$(whoami)" != "root" ] ; then
+  echo "This script requires superuser privileges" 1>&2
+  exit
+fi
+
+calibrator_start () {
+  $calibrator_command &
+  calibrator_process=$!
+}
+
+calibrator_stop () {
+  kill -int $calibrator_process
+}
+
+calibrator_running () {
+  if [ "$(ps -p $calibrator_process -o command=)" == "$calibrator_command" ] ; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+calibrator_start
+
+while ! $interrupted ; do
+  if ! calibrator_running ; then
+    break
+  fi
+done
+
+if calibrator_running ; then
+  calibrator_stop
+fi
+
+while calibrator_running ; do
+  sleep 0
+done
